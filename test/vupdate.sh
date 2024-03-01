36a956f6a6925ebcf8092b38ce732d9e872d56c3
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-03-01 12:55:09 +0530

    bumo version to 0.3.0

--- test/vupdate.sh
+++ test/vupdate.sh
@@ -12,17 +12,17 @@ NEW=$2
 
 grep "VN:$OLD" test/rna_paf.sam.exp || die "Version $OLD not found in test/rna_paf.sam.exp"
 grep "VN:$OLD" test/dna_r10_paf-ref.sam.exp || die "Version $OLD not found in dna_r10_paf-ref.sam.exp"
-grep "SQ_VERSION \"$OLD\"" src/sq.h || die "Version $OLD not found in src/sq.h"
+grep "SQ_VERSION \"$OLD\"" src/version.h || die "Version $OLD not found in src/sq.h"
 grep "VERSION=$OLD" README.md || die "Version $OLD not found in README.md"
 
 sed -i "s/VN:$OLD/VN:$NEW/" test/rna_paf.sam.exp | grep "VN:$NEW"
 sed -i "s/VN:$OLD/VN:$NEW/" test/dna_r10_paf-ref.sam.exp | grep "VN:$NEW"
-sed -i "s/SQ_VERSION \"$OLD\"/SQ_VERSION \"$NEW\"/" src/sq.h | grep "SQ_VERSION \"$NEW\""
+sed -i "s/SQ_VERSION \"$OLD\"/SQ_VERSION \"$NEW\"/" src/version.h | grep "SQ_VERSION \"$NEW\""
 sed -i "s/VERSION=$OLD/VERSION=$NEW/" README.md | grep "VERSION=$NEW"
 
 grep "VN:$NEW" test/rna_paf.sam.exp || die "updated version $NEW not found in test/rna_paf.sam.exp"
 grep "VN:$NEW" test/dna_r10_paf-ref.sam.exp || die "updated Version $NEW not found in dna_r10_paf-ref.sam.exp"
-grep "SQ_VERSION \"$NEW\"" src/sq.h || die "updated Version $NEW not found in src/sq.h"
+grep "SQ_VERSION \"$NEW\"" src/version.h || die "updated Version $NEW not found in src/sq.h"
 grep "VERSION=$NEW" README.md || die "updated Version $NEW not found in README.md"
 
 
